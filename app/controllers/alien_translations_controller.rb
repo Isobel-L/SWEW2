02a00915f6139e09a395a5d98b52349775085883
Thewisking <wissamdiab122@gmail.com>
2025-09-26 04:12:59 +0930

    made the create method use facade. similar to last commit

--- app/controllers/alien_translations_controller.rb
+++ app/controllers/alien_translations_controller.rb
@@ -17,27 +17,16 @@ class AlienTranslationsController < ApplicationController
   end
 
   def create
-    solution  = session[:solution]
-    scrambled = session[:scrambled]
-    return redirect_to(alien_translation_path, alert: "No active puzzle.") if solution.blank? || scrambled.blank?
+    return redirect_to(alien_translation_path, alert: "No active puzzle.") if session[:solution].blank? || session[:scrambled].blank?
+    facade  = AlienTranslations::AlienTranslationsFacade.new(session: session)
+    correct = facade.submit_guess!(params[:attempt])
 
-    hint_mgr = ::AlienTranslations::Hints::ProgressiveHintManager.new
-    @puzzle  = Puzzle.new(solution: solution, scrambled_word: scrambled, hint_manager: hint_mgr)
-
-    session[:attempts] = session[:attempts].to_i + 1
-
-    service = ::AlienTranslations::GameService.new(
-      word_repository: ::Repositories::StaticWordRepository.new,
-      scrambler: ::AlienTranslations::Scramblers::ShuffleScrambler.new,
-      validator: ::AlienTranslations::Validators::ExactMatchValidator.new,
-      hint_manager: hint_mgr
-    )
-
-    if service.check_attempt(@puzzle, params[:attempt])
-      redirect_to alien_translation_path, notice: "Correct! The word was #{solution}."
+    if correct
+      redirect_to alien_translation_path, notice: "Correct! The word was #{session[:solution]}."
     else
       flash.now[:alert] = "Try again!"
-      @hint = ::AlienTranslations::Hints::SessionProgressStore.new(session).last_hint
+      @puzzle = facade.current_puzzle
+      @hint = facade.last_hint
       render :alien_translation, status: :unprocessable_entity
     end
   end
