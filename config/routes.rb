ddbdefe134781530a32cbd65ebd5a100150fd2f9
Luke <lukehdonnelly@gmail.com>
2025-10-13 14:53:13 +1030

    Number Generation Testing

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get "account", to: "pages#account", as: :account
   patch "account", to: "pages#update_account"
 
+  get "blast_off", to: "blast_off#show"
+  post "blast_off/check", to: "blast_off#check"
+
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
