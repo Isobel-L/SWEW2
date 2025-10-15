19cfaebedee6a055bac34a1fb5151e7edd8f3504
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-10-15 23:17:31 +1030

    added access to login page with /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
-
+ # Routes
   get "profile", to: "pages#profile", as: :profile
   get "account", to: "pages#account", as: :account
   patch "account", to: "pages#update_account"
-
+  get "login", to: "pages#login", as: :login #Just /login to access for now
   get "up" => "rails/health#show", as: :rails_health_check
 end
