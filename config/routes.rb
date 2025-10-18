0c5fbf0a0d2876c38f91a2d171374b97107a7b55
Thewisking <wissamdiab122@gmail.com>
2025-10-18 10:35:49 +1030

    merged blast off and fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  resource  :blast_off, only: :show
+  # --- General pages ---
   get "pages/home"
   get "home/about"
   get "home/index"
 
+  # --- Alien Translation game ---
   get  "alien_translation", to: "alien_translations#alien_translation", as: :alien_translation
   post "alien_translation", to: "alien_translations#create"
   post "alien_translation/hint", to: "alien_translations#hint", as: :alien_translation_hint
@@ -13,20 +14,23 @@ Rails.application.routes.draw do
        to: "alien_translations#change_difficulty",
        as: :change_difficulty_alien_translation
 
+  # --- Blast Off game ---
+  get  "blast_off",        to: "blast_off#show",  as: :blast_off
+  post "blast_off/check",  to: "blast_off#check", as: :blast_off_check
+
+  # --- Account and Profile ---
   get   "account", to: "pages#account",       as: :account
   patch "account", to: "pages#update_account"
-  get "profile", to: "pages#home", as: :profile
+  get   "profile", to: "pages#home",          as: :profile
 
+  # --- Authentication ---
   get    "login",  to: "sessions#new"
   post   "login",  to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+
   get  "signup", to: "users#new"
   post "signup", to: "users#create"
 
-  get "blast_off", to: "blast_off#show"
-  post "blast_off/check", to: "blast_off#check"
-
-
+  # --- Health check ---
   get "up" => "rails/health#show", as: :rails_health_check
-  
 end
